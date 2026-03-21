0dbfe342684c0d875b031101efca2dcdb96c4293
EmYours <129309187+EmYours@users.noreply.github.com>
2026-03-21 14:09:46 +0800

    Deployment Fix

--- web-app/src/pages/CustomerBookings.js
+++ web-app/src/pages/CustomerBookings.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Axios from 'axios';
 import { Search, ChevronLeft, ChevronRight, Filter, CreditCard } from 'lucide-react';
 import './PortalStyles.css';
