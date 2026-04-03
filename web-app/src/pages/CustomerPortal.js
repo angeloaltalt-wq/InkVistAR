f7bfeee26227c83b0f6ac313bc65474bf7232604
Clarence Celis <clarencecelis.2004@gmail.com>
2026-04-04 00:04:07 +0800

    Final fix for real for the UI of the customer dashboard.

--- web-app/src/pages/CustomerPortal.js
+++ web-app/src/pages/CustomerPortal.js
@@ -162,7 +162,7 @@ function CustomerPortal() {
                     </div>
                 </header>
 
-                <p className="header-subtitle" style={{ marginTop: '-3.5rem', marginBottom: '2.5rem', marginRight: '-5.5rem', textAlign: 'left' }}>Welcome back, {customer.name || 'Inker'}!</p>
+                <p className="header-subtitle" style={{ marginTop: '-3rem', marginBottom: '2.5rem', marginRight: '-5.5rem', textAlign: 'left' }}>Welcome back, {customer.name || 'Inker'}!</p>
 
                 <div className="portal-content">  
                     {loading ? (
