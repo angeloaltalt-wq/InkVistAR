26c1934f73ec9e9a4dee26ee70b8b498823b6bdc
Clarence Celis <clarencecelis.2004@gmail.com>
2026-04-04 00:05:03 +0800

    Really fixed it manually for the customer dashboard.

--- web-app/src/pages/CustomerPortal.js
+++ web-app/src/pages/CustomerPortal.js
@@ -162,7 +162,7 @@ function CustomerPortal() {
                     </div>
                 </header>
 
-                <p className="header-subtitle" style={{ marginTop: '-3rem', marginBottom: '2.5rem', marginRight: '-5.5rem', textAlign: 'left' }}>Welcome back, {customer.name || 'Inker'}!</p>
+                <p className="header-subtitle" style={{ marginTop: '-1rem', marginBottom: '2.5rem', marginRight: '-5.5rem', textAlign: 'left' }}>Welcome back, {customer.name || 'Inker'}!</p>
 
                 <div className="portal-content">  
                     {loading ? (
